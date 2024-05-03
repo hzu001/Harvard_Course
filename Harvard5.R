5b58e21ef33ddd42f347f5a371b66bdf301ff9c5
hzu001 <you@example.com>
2024-05-03 20:35:50 +0200

    Committing my first file

---
+++ Harvard5.R
@@ -0,0 +1,14 @@
+
+######### Productivity tools ##########
+
+
+#### Section 1: Installing software ######
+
+
+install.packages("dslabs")  # to install a single package
+install.packages(c("tidyverse", "dslabs")) # to install two packages at the same time
+installed.packages() # to see the list of all installed packages
+
+library(tidyverse)
+
+setwd("C:\\Users\\Hendrik Z\\Desktop\\R-course Harvard")
