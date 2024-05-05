ce5f338e51eafd589eddba26ebd52263387cfdb7
hzu001 <you@example.com>
2024-05-05 18:37:23 +0200

    First time uploading Harvard6

---
+++ Harvard6.R
@@ -0,0 +1,99 @@
+
+
+######### Data wrangling ##########
+
+
+#### Section 1: Data Import #####
+
+
+# see working directory
+getwd()
+
+# change your working directory
+setwd()
+
+# set path to the location for raw data files in the dslabs package and list files
+path <- system.file("extdata", package="dslabs")
+list.files(path)
+
+# generate a full path to a file
+filename <- "murders.csv"
+fullpath <- file.path(path, filename)
+fullpath
+
+# copy file from dslabs package to your working directory
+file.copy(fullpath, getwd())
+
+# check if the file exists
+file.exists(filename)
+
+
+
+library(dslabs)
+library(tidyverse)    # includes readr
+library(readxl)
+
+# inspect the first 3 lines
+read_lines("murders.csv", n_max = 3)
+
+# read file in CSV format
+dat <- read_csv(filename)
+
+#read using full path
+dat <- read_csv(fullpath)
+head(dat)
+
+#Ex：
+path <- system.file("extdata", package = "dslabs")
+files <- list.files(path)
+files
+
+filename <- "murders.csv"
+filename1 <- "life-expectancy-and-fertility-two-countries-example.csv"
+filename2 <- "fertility-two-countries-example.csv"
+dat=read.csv(file.path(path, filename))
+dat1=read.csv(file.path(path, filename1))
+dat2=read.csv(file.path(path, filename2))
+
+
+
+
+# filename is defined in the previous video
+# read.csv to import the data
+dat2 <- read.csv(filename)
+class(dat2$abb)
+class(dat2$region)
+
+
+
+url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dat <- read_csv(url)
+download.file(url, "murders.csv")
+tempfile()
+tmp_filename <- tempfile()
+download.file(url, tmp_filename)
+dat <- read_csv(tmp_filename)
+file.remove(tmp_filename)
+
+
+
+file.location <- file.path(system.file("extdata", package = "dslabs"), "murders.csv")
+file.destination <- file.path(getwd(), "data")
+file.copy(file.location, file.destination)
+
+
+
+#### Assessment 1 #######
+
+library(tidyverse)
+url <- url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+dat <- read_csv(col_names=FALSE,url)
+nrow(dat)
+ncol(dat)
+
+
+
+#### Section 2: Tidy data #####
+
+
+
